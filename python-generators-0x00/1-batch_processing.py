a0413d3676bd5dd6c7ac62e14506e550363a24f4
USER <darren.moty@rentoza.co.za>
2025-07-06 23:22:38 +0200

    adding yield generator to batch processing

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -6,9 +6,7 @@ def stream_users_in_batches(batch_size):
 
     offset = 0
     while True:
-        cursor.execute(
-            "SELECT * FROM user_data LIMIT %s OFFSET %s", (batch_size, offset)
-        )
+        cursor.execute("SELECT * FROM user_data LIMIT %s OFFSET %s", (batch_size, offset))
         rows = cursor.fetchall()
         if not rows:
             break
