31ad08fe9c5ed7c062036881ce057acf44d1205f
USER <darren.moty@rentoza.co.za>
2025-07-06 23:20:38 +0200

    adding yield generator to batch processing

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,13 +1,9 @@
-import mysql.connector
 from seed import connect_to_prodev
 
 def stream_users_in_batches(batch_size):
-    """
-    Generator that yields user data in batches from the user_data table.
-    """
     connection = connect_to_prodev()
     cursor = connection.cursor(dictionary=True)
-    
+
     offset = 0
     while True:
         cursor.execute(
@@ -23,10 +19,7 @@ def stream_users_in_batches(batch_size):
     connection.close()
 
 def batch_processing(batch_size):
-    """
-    Process each batch by filtering users older than 25 and printing them.
-    """
-    for batch in stream_users_in_batches(batch_size):  # Loop 1
-        for user in batch:  # Loop 2
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
             if user['age'] > 25:
                 print(user)
